8c7c08f1719e1fab81c289caa3e7ff2df7e69018
VineetKundu <kunduvineet6@gmail.com>
2025-03-06 14:49:48 +0530

    changes done

--- app/src/main/java/com/teeniv/whatsapp/Adapters/ChatAdapter.java
+++ app/src/main/java/com/teeniv/whatsapp/Adapters/ChatAdapter.java
@@ -2,7 +2,6 @@ package com.teeniv.whatsapp.Adapters;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
